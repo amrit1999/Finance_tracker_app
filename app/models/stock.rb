cd9dea742281d26bc5aa513f9828d74f4c50ae82
amrit1999 <amrit.cpr@gmail.com>
2020-08-04 00:23:23 +0530

    ajax and javascript added

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,4 @@
-class Stock < ApplicationRecord
+ class Stock < ApplicationRecord
 
 	def self.new_lookup(ticker_symbol)
 		client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_publishable_key],
